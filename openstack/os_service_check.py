7bf53f292f947ba9e8e53c4698c0d0794cd7c473
aneeshkp <aneeshputtur@gmail.com>
2017-02-21 11:25:52 -0500

    added tabular format printing results on failure

--- openstack/os_service_check.py
+++ openstack/os_service_check.py
@@ -1,13 +1,18 @@
 from keystoneclient.v2_0 import client
 import requests
 import os
-
-
+import eventlet
+import datetime
+import time
+from time import sleep
+from prettytable import PrettyTable
 def check_keystone():
-    allowed_values=['identity','volume','image''glance','compute','neutron','network']
+    allowed_values=['identity','volume','image','glance','compute','neutron','network']
     internal_url={}
     admin_url={}
     service_types=[]
+    service_status={}
+    results={} 
     keystone = client.Client(username=os.environ['OS_USERNAME'],
                              password=os.environ['OS_PASSWORD'],
                              tenant_name=os.environ['OS_TENANT_NAME'],
@@ -17,6 +22,8 @@ def check_keystone():
         print service['type']
         if service['type'] in allowed_values:
             service_types.append(service["type"])
+            service_status[service["type"]]="UNKNOW"
+            results[service["type"]]={}
             internal_url[service['type']] = service['endpoints'][0]['internalURL']
             admin_url[service['type']] = service['endpoints'][0]['adminURL']
     
@@ -29,14 +36,54 @@ def check_keystone():
     }
 
     if service_types:
-	    print "----------------------------------------"
-            for service_type in service_types:
-                print service_type
-                print "----------------------------------------"
-                print admin_url[service_type]
-	        print requests.get(admin_url[service_type])
-                print requests.get(admin_url[service_type]).elapsed.total_seconds()
-                print "----------------------------------------"
+        print "----------------------------------------"
+    try:
+       status_changed=True
+       while True:
+            table = PrettyTable(['Service', 'staus','e-time','reason','last_s_time','last_status'])
+       	    for service_type in service_types:
+                result={} 
+       	        with eventlet.Timeout(1):
+   	     	 response= requests.get(admin_url[service_type])
+                 if service_status[service_type]!=response.status_code:
+                    service_status[service_type]=response.status_code
+                    status_changed=True
+                    result["service_name"]=service_type
+                    result["status"]=response.status_code
+                    result["elapsed_time"]=response.elapsed.total_seconds()
+                    result["reason"]=response.reason
+                    if not results[service_type]:
+                         result["last_success_time"]=str(datetime.datetime.now()) 
+                         result["last_status"]="UNKNOW"
+                    else:
+                        result["last_success_time"]=results[service_type].last_success_time
+                        result["last_status"]=results[service_type].last_status
+                    table.add_row([result["service_name"],result["status"], result["elapsed_time"],result["reason"],result["last_success_time"],result["last_status"]])
+                    results[service_type]=result
+                 else:
+                    result=results[service_type]
+                    table.add_row([result["service_name"],result["status"], result["elapsed_time"],result["reason"],result["last_success_time"],result["last_status"]])
+                    
+
+            if status_changed:
+                 print table
+                 status_changed=False
+	    sleep(0.10)
+    except KeyboardInterrupt:
+	 sleep(5)
+	 print 'interrupted!'
+                      
+                #print"Internal URL==> "+  internal_url[service_type]
+               # response= requests.get(internal_url[service_type])
+               # print response.content
+               # print response.elapsed.total_seconds()
+               # print response.reason
+               # print response.status_code
+               # print response.url
+               # print"Internal URL==> "+  internal_url[service_type]
+
+                 #  print requests.get(internal_url[service_type]).elapsed.total_seconds()
+                #print "----------------------------------------"
       
        
 
